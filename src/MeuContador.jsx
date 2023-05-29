55a8fe775da994aa591c32da1b3430cc76329e8e
Leonardo Zabeu <104705213+Tjl-z@users.noreply.github.com>
2023-05-28 22:22:41 -0300

    segundo commit

--- src/MeuContador.jsx
+++ src/MeuContador.jsx
@@ -6,15 +6,25 @@ export default function MeuContador() {
   console.log('renderizando contador...')
   const [contador, setContador] = useState(0)
 
+ 
   function aumentar() {
     setContador(contador + 1)
+  }
+
+  function diminuir() {
+    setContador(contador - 1)
   }
 
   return (
     <div className="container">
       <h1>Meu Contador</h1>
-      <h3>Meu contador: {contador}</h3>
-      <button className={styles.myButton} onClick={aumentar}>aumentar</button>
+      <h3>{contador}</h3>
+
+      <div className="button">
+      <button className={styles.myButton} onClick={aumentar}>Aumentar</button>
+      <button className={styles.myButton} onClick={diminuir}>Diminuir</button>
+      </div>
+
     </div>
   )
 }
